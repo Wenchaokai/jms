44c98a9039a9158e32cfa98ec4f7bbbc4510cd3e
Wenchaokai <wenchaokai@gmail.com>
2013-12-22 10:26:20 +0800

    Initial commit

---
+++ configs/modules.php
@@ -0,0 +1,6 @@
+<?php
+$jieqiModules['system'] = array('caption'=>'系统功能', 'dir'=>'', 'path'=>'', 'url'=>'', 'theme'=>'', 'publish'=>'1');
+$jieqiModules['article'] = array('caption'=>'小说连载', 'dir'=>'', 'path'=>'', 'url'=>'', 'theme'=>'', 'publish'=>'0');
+$jieqiModules['obook'] = array('caption'=>'电子书店', 'dir'=>'', 'path'=>'', 'url'=>'', 'theme'=>'', 'publish'=>'0');
+$jieqiModules['pay'] = array('caption'=>'在线充值', 'dir'=>'', 'path'=>'', 'url'=>'', 'theme'=>'', 'publish'=>'0');
+?>
